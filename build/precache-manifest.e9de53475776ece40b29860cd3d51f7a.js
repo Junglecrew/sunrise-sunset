27bc585ce7568968db9c4a472d66ddaa629cc98e
Dmitri Petrov <junglecrew@gmail.com>
2020-03-22 17:30:21 +0200

    build

---
+++ build/precache-manifest.e9de53475776ece40b29860cd3d51f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06871847d7ce483e302c97a2a13e66d2",
+    "url": "/sunrise-sunset/index.html"
+  },
+  {
+    "revision": "f76b4e165cff994355e9",
+    "url": "/sunrise-sunset/static/css/main.1583bfd4.chunk.css"
+  },
+  {
+    "revision": "71d59d93f7fa5db1e73e",
+    "url": "/sunrise-sunset/static/js/2.ef28e825.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sunrise-sunset/static/js/2.ef28e825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76b4e165cff994355e9",
+    "url": "/sunrise-sunset/static/js/main.9e2a7c0c.chunk.js"
+  },
+  {
+    "revision": "3c4bc443882a203cfe02",
+    "url": "/sunrise-sunset/static/js/runtime-main.f4fd1bda.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53157c004acba9757d5714f150d6f2c5.js"
+  "/sunrise-sunset/precache-manifest.e9de53475776ece40b29860cd3d51f7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sunrise-sunset/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
